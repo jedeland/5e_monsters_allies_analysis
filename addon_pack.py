9fa6421315475e5caa3cb9a3ad57ed81320da1df
jedel <jedeland@live.co.uk>
2020-01-13 21:06:26 +0100

    Updated and refactored witcherify monster file

--- addon_pack.py
+++ addon_pack.py
@@ -23,12 +23,12 @@ def witcherify_monster():
         print("Your group is made up of players that are levels : ", level_inputs,
               "\nThat means the average level is {}".format(average_lvl),
               "\nThe witcherified monster should be CR: {}".format(cr_out))
-        monster = choose_monster(cr_out)
+        monster = refine_monster(cr_out) #Continue after function is completed
     else:
         print("This is an invalid input, please ensure the input is numeric and above 1")
         pass
 
-def choose_monster(cr_in):
+def refine_monster(cr_in):
     #This function aims to choose a random monster based on the CR set above
     df_copy = monster_analysis.standard_dev_cr() #Uses standard dev to create a more relevant df
     df_refined = df_copy[round(df_copy["cr"]) == cr_in]
@@ -41,16 +41,31 @@ def choose_monster(cr_in):
         monster_list.pop(np.random.randint(len(monster_list)))
     print(monster_list)
     print("Your options for a witcherfied monster are: ")
-    print(df_refined.keys())
+    #print(df_refined.keys())
+    monster_selections = []
+    refine_selections(df_refined, monster_list, monster_selections)
+    print("Your options of selection are:")
+    print("1: {0} \n2: {1} \n3: {2}".format(monster_selections[0], monster_selections[1], monster_selections[2]))
+    monster_choice = input("Please type the number of your preferred monster, from the list: ")
+    monster_list = monster_list.drop(monster_list[monster_list["name"] != mon])
+
+
+
+
+
+
+
+def refine_selections(df_refined, monster_list, monster_selections):
     for i in range(len(monster_list)):
         monster_row = df_refined[df_refined.index == monster_list[i]]
         print(monster_row[["name", "hp", "ac", "size", "src", "pagenum", "section", "environment", "alignment"]])
+        print("A {0} is a CR {1} monster with {2} hp that lives in the: {3}".format
+              (monster_row["name"].values, monster_row["cr"].values,
+               monster_row["hp"].values, monster_row["environment"].values))
+        monster_selections.append("A {0} is a CR {1} monster with {2} hp that lives in the: {3}".format
+              (monster_row["name"].values, monster_row["cr"].values,
+               monster_row["hp"].values, monster_row["environment"].values))
         print("\n***\n")
-
-
-    #print(df_refined.head())
-
-
 
 
 witcherify_monster()
