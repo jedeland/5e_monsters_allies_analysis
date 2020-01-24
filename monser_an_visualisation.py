1369e494fed50d056fa8366a7d56c7d081a8c204
jedel <jedeland@live.co.uk>
2020-01-24 11:33:31 +0100

    Create monser_an_visualisation.py

---
+++ monser_an_visualisation.py
@@ -0,0 +1,7 @@
+import pandas as pd; import numpy as np;
+import sqlalchemy as sql; import string; import import_data; import monster_analysis
+#Pandas and sqlalchemy have to be initialised with the terminal, use pip install x to do so
+
+'''
+This file aims to visualise data that has been processed in the monster_analysis.py file
+'''
