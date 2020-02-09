5804e44813596fb09929fde75a45ad0c85caddcc
jedel <jedeland@live.co.uk>
2020-02-09 13:33:20 +0100

    Updated interface

---
+++ interface.py
@@ -0,0 +1,5 @@
+#No imports are required for this file, as the file will be used to direct a textual interface
+def start():
+    user_in = input("Please type the action you wish to take")
+    actions_dict = {}
+    analysis_list = ["Find Average", "Find IQR", "Find Types of Monsters", "General Information", "Show Monsters in IQR", "Rank Monsters By Cr", "Show Monsters in Standard Deviation"]
