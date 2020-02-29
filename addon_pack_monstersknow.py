7460a958f01d9cb3cb5550f6ebaae3c051b08439
jedel <jedeland@live.co.uk>
2020-02-29 12:08:10 +0100

    Added code to store text data in dict

--- addon_pack_monstersknow.py
+++ addon_pack_monstersknow.py
@@ -9,24 +9,31 @@ This can be done by using beautiful soup to scrape information from each article
 
 def read_blogs():
     page_urls = ["cr-1-4", "cr-1-2"]
+    func_list, entry_dict = [], {} #list will be passed to another function to read sub pages
     for i in range(21):
-        page_urls.append("cr-{}".format(i+1))
+        page_urls.append("cr-{}".format(i+1)) #Automating target pages
     print(page_urls)
 
-    for i in range(len(page_urls)-15): #Is currently set to the first 6 entries for levities sake
+    for i in range(len(page_urls)-15): #Is currently set to the first 6 entries for speed
         address = "http://themonstersknow.com/tag/{}/".format(page_urls[i])
         file = requests.get(address)
         print(address)
         soup = BeautifulSoup(file.content, "html.parser")
         read_data = soup.find_all("div", class_="entry-content")
         #Needs to follow link found in <a > tag, so that complete page is shown
-        b = 0 #Crude Iterator
+        b = 0 #Local Iterator
         for item in read_data:
-            b = b + 1
-            x = b
-            #print(x, item.p, "\n", item.a)
-            if item.a is not None:
+            b = b + 1; x = b
+            print(x, item.get_text(), "\n ", item.a)
+            if item.a is None:
+                entry_dict = {str(item.strong.text) : str(item.get_text())}
+                print(entry_dict)
+            elif item.a is not None:
                 print(item.a.get("href"))
+                func_list.append(item.a.get("href"))
+
+    func_list = list(dict.fromkeys(func_list))
+    print("Testing: {}".format(func_list), "\n ", entry_dict)
 
 
 
