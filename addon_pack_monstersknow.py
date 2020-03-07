b8cab82047ed585bf5f81b35b09f82ce1ab79540
jedel <jedeland@live.co.uk>
2020-03-07 16:23:38 +0100

    small update to create SQL and Excel versions of dataframe

--- addon_pack_monstersknow.py
+++ addon_pack_monstersknow.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import, division, print_function
-import pandas as pd; import numpy as np
-import requests; import os.path; import re; import collections
+import pandas as pd; import numpy as np; from sqlalchemy import create_engine, sql
+import requests; import os.path;
 import nltk as nltk
 from nltk.corpus import stopwords; from nltk.cluster.util import cosine_distance
 import networkx as nx
@@ -13,17 +13,9 @@ And pushing that through a machine learning function
 '''
 
 
-def clean_data(df):
-    print("Starting up the machine learning, this should either be done by scrapping the site using beautiful soup, or via an SQL or CSV file that has been printed to and read from")
-    df_targ = df.loc[df["article_id"] == "Angel Tactics"] #Test case using one article, future case will use for loop and iteration, reminder https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
-    article = str(df_targ["text"].values)
-    sections = article.split(".Next: ")
-    text = str(sections[0])
-    #read_article(text)
-    gen_summary(text, 5)
-    #Line divides the article in two, removing the "share, save ect ect" part
 
 #The below function references the following article, which is being used as a basis for the implementation for NLTK usage
+#The below functions relate to the NLTK ML library
 #https://towardsdatascience.com/understand-text-summarization-and-create-your-own-summarizer-in-python-b26a9f09fc70
 def read_article(text):
     print("Reading : {}".format(text))
@@ -38,7 +30,7 @@ def read_article(text):
 
     return sentence_lst
 
-def gen_summary(article, top_n=5):
+def gen_summary(article, top_n):
     stop_words = stopwords.words("english")
     summarize_text = []
     #Tokenize text
@@ -90,55 +82,77 @@ def sentence_similarity(sent_id_1, sent_id_2, stopwords=None):
     return 1 - cosine_distance(vec1, vec2)
 
 
-
+#The below functions relate to reading and assigning articles to different sections
 def read_blogs():
-    page_urls = ["cr-1-4", "cr-1-2"]
-    print("Reading blogs this may take a while.")
-    url_list, entry_dict = [], {} #list will be passed to another function to read sub pages
-    for i in range(21):
-        page_urls.append("cr-{}".format(i+1)) #Automating target pages
-    print(page_urls)
+    if os.path.exists("articles.xlsx"):
+        print("Using preexisting excell sheet instead")
+    else:
+        page_urls = ["cr-1-4", "cr-1-2"]
+        print("Reading blogs this may take a while.")
+        url_list, entry_dict = [], {} #list will be passed to another function to read sub pages
+        for i in range(21):
+            page_urls.append("cr-{}".format(i+1)) #Automating target pages
+        print(page_urls)
 
-    for i in range(len(page_urls)): #Is currently set to the first 6 entries for speed
-        address = "http://themonstersknow.com/tag/{}/".format(page_urls[i])
-        file = requests.get(address)
-        print(address)
-        soup = BeautifulSoup(file.content, "html.parser")
-        read_data = soup.find_all("div", class_="entry-content")
-        #Needs to follow link found in <a > tag, so that complete page is shown
-        b = 0 #Local Iterator
-        for item in read_data:
-            b = b + 1; x = b
-            print(x, item.get_text(), "\n ", item.a)
-            if item.a is None:
-                text_entry = item.get_text()
-                text_entry = text_entry.replace("\n", "")
-                entry_dict[str(item.strong.text)] =  text_entry #This should make keys and values that work without the need to manually assign them
+        for i in range(len(page_urls)): #Is currently set to the first 6 entries for speed
+            address = "http://themonstersknow.com/tag/{}/".format(page_urls[i])
+            file = requests.get(address)
+            print(address)
+            soup = BeautifulSoup(file.content, "html.parser")
+            read_data = soup.find_all("div", class_="entry-content")
+            #Needs to follow link found in <a > tag, so that complete page is shown
+            b = 0 #Local Iterator
+            for item in read_data:
+                b = b + 1; x = b
+                print(x, item.get_text(), "\n ", item.a)
+                if item.a is None:
+                    text_entry = item.get_text()
+                    text_entry = text_entry.replace("\n", "")
+                    entry_dict[str(item.strong.text)] =  text_entry #This should make keys and values that work without the need to manually assign them
 
-            elif item.a is not None:
-                print(item.a.get("href"))
-                if "http://" not in item.a.get("href"):
-                    pass
-                else:
-                    url_list.append(item.a.get("href"))
-    print("Reading sub lists")
-    url_list, entry_dict = read_sublinks(url_list, entry_dict)
+                elif item.a is not None:
+                    print(item.a.get("href"))
+                    if "http://" not in item.a.get("href"):
+                        pass
+                    else:
+                        url_list.append(item.a.get("href"))
+        print("Reading sub lists")
+        url_list, entry_dict = read_sublinks(url_list, entry_dict)
 
-    url_list = list(dict.fromkeys(url_list))
-    print("Testing: {}".format(url_list), "\n ")
-    print("formed dictionary of articles ", entry_dict.keys())
-    df = form_df(entry_dict)
-    clean_data(df)
+        url_list = list(dict.fromkeys(url_list))
+        print("Testing: {}".format(url_list), "\n ")
+        print("formed dictionary of articles ", entry_dict.keys())
+        df = form_df(entry_dict)
+        clean_data(df)
 
 
+def clean_data(df):
+    print("Starting up the machine learning, this should either be done by scrapping the site using beautiful soup, or via an SQL or CSV file that has been printed to and read from")
+    df_targ = df.loc[df["article_id"] == "Angel Tactics"] #Test case using one article, future case will use for loop and iteration, reminder https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas
+    article = str(df_targ["text"].values)
+    sections = article.split(".Next: ")
+    text = str(sections[0])
+    sent_list = read_article(text)
+    num_sum = round(len(sent_list) * .30)
+
+    #read_article(text)
+    gen_summary(text, num_sum)
 
 def form_df(entry_dict):
     df_mk = pd.DataFrame(columns=["article_id", "text"])
     for k, v in sorted (entry_dict.items()):
         print(k, "is the key for: ", v)
         df_mk = df_mk.append({"article_id": k, "text": v}, ignore_index=True)
-    print(df_mk)
+    form_sql(df_mk)
+    df_mk.to_excel("articles.xlsx", sheet_name="monstersknow")
     return df_mk
+
+
+def form_sql(df_mk):
+    print(df_mk)
+    eng = create_engine('sqlite://', echo=False)
+    df_mk.to_sql("articles", con=eng)
+    eng.execute("SELECT * FROM articles").fetchall()
 
 
 def read_sublinks(func_list, entry_dict):
